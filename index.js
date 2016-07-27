d2fb6df53f25ef31812da035c743d6b998b18d9b
Joseph Daniels <joedaniels29@me.com>
2016-07-26 20:57:47 -0400

    should be updated for latest ember

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ var mergeTrees = require('broccoli-merge-trees');
 var pickFiles = require('broccoli-static-compiler');
 
 module.exports = {
-  name : 'ember-cli-textillate',
-  afterInstall : function() {
-
-    return this.addBowerPackageToProject('textillate');
-  },
-  included : function(app) {
+  name: 'ember-cli-textillate',
+  included: function(app) {
     this._super.included(app);
     app.import('bower_components/textillate/jquery.textillate.js');
   }
