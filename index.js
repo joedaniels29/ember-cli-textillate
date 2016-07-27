df18fe357ecce52d8aa0a8509365b410df37b9d7
Joseph Daniels <joedaniels29@me.com>
2016-07-26 21:00:26 -0400

    should be updated for latest ember

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 'use strict';
-var path = require('path');
-var checker = require('ember-cli-version-checker');
-var mergeTrees = require('broccoli-merge-trees');
-var pickFiles = require('broccoli-static-compiler');
 
 module.exports = {
   name: 'ember-cli-textillate',
